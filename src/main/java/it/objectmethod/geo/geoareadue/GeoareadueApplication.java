01bd3ef440ed73d242a744d670e13582d7d0eec2
Emmanuel Bellissimo <emmanuel.bellissimo@objectmethod.it>
2019-03-04 18:09:17 +0100

    primo commit

---
+++ src/main/java/it/objectmethod/geo/geoareadue/GeoareadueApplication.java
@@ -0,0 +1,13 @@
+package it.objectmethod.geo.geoareadue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoareadueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoareadueApplication.class, args);
+	}
+
+}
